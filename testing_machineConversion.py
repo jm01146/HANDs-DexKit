bc4fb99fb40cfcef0d30381d2e8c857da8206e6e
John S. Marcial-Manjarres <59844100+jm01146@users.noreply.github.com>
2025-08-20 11:03:01 -0400

    Create testing_machineConversion.py
    Relating pixel dimension of GUI and Camera to X, Y coordinates of a CNC machine

---
+++ testing_machineConversion.py
@@ -0,0 +1,21 @@
+"""
+Trying to convert GUI pixels to machine code 
+"""
+
+# GUI parameters 
+gui_width = 2250  # pixels
+gui_heigth = 1500  # pixels
+
+# Gantry Config
+gantry_width = 600 # mm
+gantry_heigth = 900 # mm
+
+# Stepper Motor config
+
+
+
+
+"""
+Gcode notes: You must start every string with J$=
+and it must follow with either a conditional code such as G
+"""
